9a6f9067dd02ed2a882221a70ca9283f15afce93
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2022-08-27 18:06:29 +0900

    env deleted

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import './style.css'
-import App from './App.vue'
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app');
--- vite.cofnig.js
+++
@@ -1,8 +0,0 @@
-import { UserConfig } from 'vite'
-import vuePlugin from '@vitejs/plugin-vue2'
-import { createVuePlugin } from 'vite-plugin-vue2'
-
-/** @type {UserConfig} */
-export default {
-  plugins: [createVuePlugin()]
-}
