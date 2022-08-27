34808d36dd89cc0c680f0dc3e4882f62dcd040c1
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2022-08-27 17:00:54 +0900

    no

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import './style.css'
+import App from './App.vue'
+
+new Vue({
+  el: App
+}).$mount('#app');
---
+++ vite.cofnig.js
@@ -0,0 +1,7 @@
+import { UserConfig } from 'vite'
+import vuePlugin from '@vitejs/plugin-vue2'
+
+/** @type {UserConfig} */
+export default {
+  plugins: [vuePlugin()]
+}
